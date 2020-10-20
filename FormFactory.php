2b4e6792b06da7987490fef3436fe6ded3c55110
Christian Lopez <lopezchristiangabriel12@gmail.com>
2020-10-20 13:38:29 +0800

    added Bonus Task

---
+++ FormFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+$html[] = FormInputFactory::create("text", ["class"=>"primary"])->setName('uname');
+$html[] = FormInputFactory::create("textArea")->setRows(4)->setName("description"); 
+ 
+foreach( $html as $ii ) {
+    echo $ii . "<br><br>";
+}
+
+Class FormInputFactory{
+	static $input;
+
+	static function create( $type, Array $attr = [] ) {
+		self::$input = $type == 'textArea' ? "<textarea ></textarea>" : "<input type='$type'>";
+
+		foreach( $attr as $atr=>$val ) {
+			self::$input = self::concatAttr( $atr, $val );
+		}
+		return new static;
+	}
+
+	function setRows( $num ) {
+		self::$input = self::concatAttr( 'rows', $num );
+		return new static;
+	}
+
+	function setName( $name ) {
+		return self::$input = self::concatAttr( 'name', $name );
+	}
+
+	static function concatAttr( $attr, $val ){
+		return substr_replace(self::$input," $attr='$val'>",strpos(self::$input, '>', 0),1);
+	}
+
+}
